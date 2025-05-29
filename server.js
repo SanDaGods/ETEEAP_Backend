aac79b55b9c5d7b29e1e0b13657ea74be720a969
MistahAaron <aaron26gatongay@gmail.com>
2025-05-29 23:45:24 +0800

    coon edit

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ mongoose.connect("mongodb://127.0.0.1:27017/Eteeap", {
   useUnifiedTopology: true,
 });
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-db.once("open", () => console.log("✅ MongoDB connected successfully"));
+const conn = mongoose.connection;
+conn.on("error", console.error.bind(console, "MongoDB connection error:"));
+conn.once("open", () => console.log("✅ MongoDB connected successfully"));
 
 // ======================
 // SCHEMAS
