056a5832b5676b52248c8e4e332aae8724ff243a
SanDaGods <christianjedhsandagan@gmail.com>
2025-06-18 13:37:41 +0800

    Update applicantController.js

--- backend/controllers/applicantController.js
+++ backend/controllers/applicantController.js
@@ -39,7 +39,8 @@ exports.register = async (req, res) => {
       return res.status(400).json({
         success: false,
         error: "Invalid email format",
-        details: Please enter a valid email address (e.g., user@example.com). Provided: ${email},
+       details: `Please enter a valid email address (e.g., user@example.com). Provided: ${email}`,
+
       });
     }
     // Check password length
